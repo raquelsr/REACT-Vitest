07b17e1618b49a2b0da7e866fc31497006221876
Raquel Serrano <raquelserranor@hotmail.com>
2023-08-11 09:33:03 +0200

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Component, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ComponentList from './pages/ComponentList'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <ComponentList />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/ComponentList.tsx
@@ -0,0 +1,7 @@
+
+export default function ComponentList() {
+    return (
+        <div>ComponentList</div>
+
+    )
+}
