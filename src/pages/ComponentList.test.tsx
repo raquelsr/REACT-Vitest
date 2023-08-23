b8f713999495ea7b0c3262a4302b121b5c74ea23
Raquel Serrano <raquelserranor@hotmail.com>
2023-08-23 18:02:34 +0200

    test: add counter and textfield tests

--- src/pages/ComponentList.test.tsx
+++ src/pages/ComponentList.test.tsx
@@ -1,14 +1,62 @@
 import { ComponentList } from './ComponentList';
-import { render, screen } from '@testing-library/react';
+import { logRoles, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+
+const user = userEvent.setup()
 
 describe('ComponentList', () => {
     it('1 to be 1', () => {
         expect(1).toBe(1);
     });
 
-    it('Render Component', () => {
+    it('render ComponentList', () => {
         render(<ComponentList />);
         expect(screen.getByText('Component List')).toBeInTheDocument();
+        expect(screen.getByRole('heading')).toHaveTextContent('Component List');
+        expect(screen.getByRole('heading', { name: 'Component List' }));
         expect(screen.getByText('Component List')).toBeTruthy();
+    });
+});
+
+describe('Counter', () => {
+    it('counter starts in 0', () => {
+        render(<ComponentList />);
+        expect(screen.getByText('Counter: 0')).toBeInTheDocument();
+        expect(screen.getByText('Counter: 0')).toBeTruthy();
+    });
+
+    it('increase counter - getByText', async () => {
+        render(<ComponentList />);
+        await user.click(screen.getByRole('button', { name: '+ 1' }));
+        expect(screen.getByText('Counter: 1')).toBeInTheDocument();
+    });
+
+    it.skip('increase counter - getByText - 2', async () => {
+        render(<ComponentList />);
+        await user.click(screen.getByRole('button', { name: '+ 1' }));
+        const counter = await screen.findByText('Counter: 1');
+        logRoles(counter)
+        expect(counter).toHaveLength(1);
+    });
+
+    it.skip('increase counter', async () => {
+        render(<ComponentList />);
+        user.click(screen.getByRole('button', { name: '+ 1' }));
+        await expect(screen.findByText('Counter: 1')).toHaveLength(1);
+    });
+
+    it('decrease counter - getByText', async () => {
+        render(<ComponentList />);
+        await user.click(screen.getByRole('button', { name: '- 1' }));
+        expect(screen.getByText('Counter: -1')).toBeInTheDocument();
+    });
+});
+
+
+describe('Textfield', () => {
+    it('write in textfield', async () => {
+        render(<ComponentList />);
+        await user.type(screen.getByRole('textbox'), 'Hello World');
+        expect(screen.getByText('Text: Hello World')).toBeInTheDocument();
     });
 });
--- src/pages/ComponentList.tsx
+++ src/pages/ComponentList.tsx
@@ -7,33 +7,31 @@ export const ComponentList = () => {
     const [text, setText] = useState('')
 
     return (
-        <>
+        <div className='component-list'>
             <h3>Component List</h3>
-            <div className='component-list'>
-                <section>
-                    <p>Counter: {count}</p>
-                    <button
-                        onClick={() => setCount((count) => count - 1)}>
-                        - 1
-                    </button>
-                    <button
-                        onClick={() => setCount((count) => count + 1)}>
-                        + 1
-                    </button>
-                </section>
-                <section>
-                    <div>Text: {text}</div>
-                    <Textfield
-                        label='Textfield'
-                        name={text}
-                        onChange={(event) => {
-                            setText(event.target.value)
-                        }}
-                        value={text}
-                        placeholder='Type...'
-                    />
-                </section>
-            </div>
-        </>
+            <section>
+                <p>Counter: {count}</p>
+                <button
+                    onClick={() => setCount((count) => count - 1)}>
+                    - 1
+                </button>
+                <button
+                    onClick={() => setCount((count) => count + 1)}>
+                    + 1
+                </button>
+            </section>
+            <section>
+                <div>Text: {text}</div>
+                <Textfield
+                    label='Textfield'
+                    name={text}
+                    onChange={(event) => {
+                        setText(event.target.value)
+                    }}
+                    value={text}
+                    placeholder='Type...'
+                />
+            </section>
+        </div>
     );
 }
